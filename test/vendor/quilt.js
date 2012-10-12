7397322204cf4d528ff3ec1b543226512412f815
Brad Dunbar <dunbarb2@gmail.com>
2012-10-12 17:12:24 -0400

    Update dependencies.

--- test/vendor/quilt.js
+++ test/vendor/quilt.js
@@ -9,15 +9,33 @@
   // Current library version.
   Quilt.VERSION = '0.0.1';
 
-  // Replace upper case characters for data attributes.
-  var dasher = /([A-Z])/g;
+  // Find dashes in attribute names.
+  var undasher = /-([a-z]|[0-9])/ig;
+
+  // Camel case data attributes.
+  var camel = function(match, letter) {
+    return (letter + '').toUpperCase();
+  };
+
+  // # Quilt.attributes
+  //
+  // Attribute handlers should be specified in camel case.  The arguments to
+  // each handler will be a DOM element and the value of the data attribute.
+  // The handler will be called with the parent view as context.
+  //
+  //     Quilt.attributes.exampleAttr = function(element, options) {
+  //       // Called for elements with a "data-example-attr" attribute.
+  //     };
+  //
+  Quilt.attributes = {};
 
   // # Quilt.View
   // Provide a structure for declaring functionality through data attributes.
   var View = Quilt.View = Backbone.View.extend({
 
-    constructor: function() {
+    constructor: function(options) {
       this.views = [];
+      if (options && options.template) this.template = options.template;
       Backbone.View.apply(this, arguments);
     },
 
@@ -25,10 +43,10 @@
     // attributes, match them with handlers and execute them.  If a handler
     // returns a view, store it for clean up.
     render: function() {
-      var el, view;
+      var elements, el, view, name, attrs, attr;
 
-      // Destroy old views.
-      while (view = this.views.pop()) if (view.destroy) view.destroy();
+      // Dispose of old views.
+      while (view = this.views.pop()) if (view.dispose) view.dispose();
 
       // Render the template if it exists.
       if (this.template) {
@@ -39,28 +57,25 @@
         }));
       }
 
-      // Create the selector if it hasn't been created already.
-      if (!Quilt.selector) {
-        Quilt.selector = _.map(_.keys(Quilt.attributes), function(attr) {
-          return '[data-' + attr.replace(dasher, '-$1').toLowerCase() + ']';
-        }).join(',');
-      }
-
-      // Find all elements with appropriate attributes.
-      var elements = this.$(Quilt.selector).get();
+      elements = this.$('*').get();
 
       // Execute the handler for each element/attr pair.
       while (el = elements.pop()) {
 
-        // Retrieve all data attributes.
-        var data = $(el).data();
+        attrs = el.attributes;
+        for (var i = 0; i < attrs.length; i++) {
 
-        for (var attr in data) {
-          // Bail for non-quilt attributes.
-          if (!Quilt.attributes[attr]) continue;
+          // Bail unless we have a data attribute.
+          if ((name = attrs[i].name).indexOf('data-') !== 0) continue;
+
+          // Camel case and strip "data-".
+          name = name.replace(/^data-/, '').replace(undasher, camel);
+
+          // Bail on non-quilt attributes.
+          if (!(attr = Quilt.attributes[name])) continue;
 
           // Execute the handler.
-          view = Quilt.attributes[attr].call(this, el, data[attr]);
+          view = attr.call(this, el, $(el).data(name));
 
           // Render the view if appropriate.
           if (view instanceof View) this.views.push(view.render());
@@ -70,34 +85,13 @@
       return this;
     },
 
-    // Destroy child views and ensure that references to this view are
-    // eliminated to prevent memory leaks.
-    destroy: function() {
-
-      // Remove DOM listeners.
-      this.undelegateEvents();
-
-      // Destroy child views.
-      _.invoke(this.views, 'destroy');
-
-      // Clean up event handlers.
-      if (this.model) this.model.off(null, null, this);
-      if (this.collection) this.collection.off(null, null, this);
-
+    // Dispose of child views.
+    dispose: function() {
+      Backbone.View.prototype.dispose.apply(this, arguments);
+      _.invoke(this.views, 'dispose');
       return this;
     }
 
   });
-
-
-  // Attribute handlers should be specified in camel case.  The arguments to
-  // each handler will be a DOM element and the value of the data attribute.
-  // The handler will be called with the parent view as context.
-  //
-  //    Quilt.attributes.exampleAttr = function(element, options) {
-  //      // Called for elements with a "data-example-attr" attribute.
-  //    };
-  //
-  Quilt.attributes = {};
 
 })();
