d6acf5a42e39ae4e2347c3015384b0479222ccc1
Brad Dunbar <dunbarb2@gmail.com>
2013-05-13 17:22:26 -0400

    Proxy child events.

--- list.js
+++ list.js
@@ -61,6 +61,9 @@
       var index = this.collection.indexOf(model);
       this.views.splice(index, 0, view);
 
+      // Proxy events
+      view.on('all', this.onViewEvent, this);
+
       view.render();
 
       // Insert the view into the DOM.
@@ -78,6 +81,9 @@
       view.$el.remove();
       view.dispose();
 
+      // Stop proxying events.
+      view.off('all', this.onViewEvent, this);
+
       // Clean up.
       delete this._views[model.cid];
       delete this._views[view.cid];
@@ -92,6 +98,10 @@
       _.each(_.difference(models, this.collection.models), this.remove, this);
       this.views = this.collection.map(this.findView, this);
       this.$el.append(_.pluck(this.views, 'el'));
+    },
+
+    onViewEvent: function() {
+      this.trigger.apply(this, arguments);
     }
 
   });
